596566cd5294744351fd44f3726edda062dd8dba
Lloyd Dsouza <lloyddsouza592@gmail.com>
2026-03-08 16:21:55 +0530

    updated api endpoint

--- pratham-app/prathamcare/lib/core/constants/app_constants.dart
+++ pratham-app/prathamcare/lib/core/constants/app_constants.dart
@@ -2,6 +2,8 @@ class AppConstants {
   AppConstants._();
 
   static const String appName = 'PrathamCare';
-  static const String apiBaseUrl =
-      String.fromEnvironment('API_BASE_URL', defaultValue: 'https://kinqjy55xg.execute-api.ap-south-1.amazonaws.com/dev');
+  static const String apiBaseUrl = String.fromEnvironment(
+    'API_BASE_URL',
+    defaultValue: 'https://15stgkxa93.execute-api.ap-south-1.amazonaws.com/dev',
+  );
 }
